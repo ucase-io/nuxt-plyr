499a0fc8f9e3935a9cc811516719da101f7bc6f2
Rinat <khusainovrm@yandex.ru>
2020-09-30 10:41:37 +0300

    added plyr functionality

---
+++ example/nuxt/pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <ucase-plyr>
+    <div class="plyr__video-embed">
+      <iframe
+        src="https://www.youtube.com/embed/bTqVqk7FSmY?iv_load_policy=3&modestbranding=1&playsinline=1&showinfo=0&rel=0&enablejsapi=1"
+        allowfullscreen
+        allowtransparency
+        allow="autoplay"
+      >
+      </iframe>
+    </div>
+  </ucase-plyr>
+</template>
+
+<script>
+export default {}
+</script>
+</style>
